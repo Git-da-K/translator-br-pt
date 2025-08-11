7c6c6c20a2e40397cdabb67e6b4117cc70213d5a
Git-da-K <k.bruna.schmidt.vieira@gmail.com>
2025-08-11 11:53:23 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,27 +3,27 @@ import time
 
 
 eng_words = ['Hi','Bye','Task', 'Programm']
-fr_words = ['Salut','Au revoir','Tâche', 'Programme']
+pt_words = ['Oi','Tchau','Tarefa', 'Programa']
 score = 0
 
-mode = input("Pick a mode: 0 - add new words, 1 - training: \n")
+mode = input("Selecione um modo: 0 - adicionar novas palavras, 1 - treinamento: \n")
 while ((mode != '0') and (mode != '1')):
-    mode = input("Invalid dymbol! Pick either 0 or 1. (0 adds new words, while 1 enables training) \n")
+    mode = input("Dymbol inválido! Escolha 0 ou 1. (0 adiciona novas palavras e 1 permite o treinamento) \n")
 
 if mode == "1":
-    print("Translate as many words as you can! You have 10 attempts!")
+    print("Traduza o máximo de palavras que puder! Você tem 10 tentativas!")
     for i in range(10):
         number = random.randint(0, len(eng_words)-1)
-        print("How should we translate: " + eng_words[number])
+        print("Como devemos traduzir: " + eng_words[number])
         if input() == fr_words[number]:
-            print("Great!!!")
+            print("Ótimo!!!")
             score += 1
         else:
-            print("Nope, not quite... The correct word is - " + eng_words[number])
+            print("Incorreto... A palavra correta é - " + eng_words[number])
 else:
-    word = input("Type in an English word: ")
-    translate = input("Type in this word's translation: ")
+    word = input("Digite uma palavra em português: ")
+    translate = input("Digite a tradução desta palavra: ")
     if len(word) > 0 and len(translate) > 0:
         eng_words.append(word)
         fr_words.append(translate)
-        print("The word was added successfully!")
+        print("A palavra foi adicionada com sucesso!")
